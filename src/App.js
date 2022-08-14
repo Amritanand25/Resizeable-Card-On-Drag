7fc448ad468fb157ee91979d79cd62234d579594
Amrit Anand <amrit.anand@s759labs.com>
2022-08-14 21:08:35 +0530

    added resizeable card ondrag

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Box from "./Components/Box";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div >
+    <div className="container">
+      {
+        Object.keys([...(new Array(3))]).map((item, i) => <Box key={i} />)
+      }
+     </div>
+   </div>
   );
 }
 
